7a9dc578bcfd6636a80ef0010b789c2f954fbb2b
GibsonCS <gibson.cruzds@gmail.com>
2025-07-25 17:57:15 -0300

    feat(contact): import contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import AboutUs from "@/components/AboutUs";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Contact from "../components/Forms/Contact";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutUs />
+      <Contact />
       <Footer />
     </>
   );
